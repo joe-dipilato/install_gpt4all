6ec9b42626ae6d900e5292fea87b2fc54a8647da
Joe DiPilato <joe.dipilato@mongodb.com>
2023-04-01 21:22:03 -0400

    udpate with whisper command

--- listen_interface.sh
+++ listen_interface.sh
@@ -11,3 +11,9 @@ source venv/bin/activate
 # numba currently doesn't support python 3.11, so downgrade
 brew install python@3.10
 python3.10 -m pip install git+https://github.com/openai/whisper.git
+
+# Record
+ffmpeg -y -f avfoundation -i ":1" out.mp3
+date +%S; whisper out.mp3 --model tiny.en -f txt --verbose False --fp16 False --language English --threads 2 --output_dir .;date +%S
+cat out.txt
+# afplay out.mp3
